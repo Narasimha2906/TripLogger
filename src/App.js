b4b5c0d4a2b25f23bb750c1226d6d235bcd3b832
Narasimha Prasad <ynprasadkklreddy2@gmail.com>
2021-07-20 17:15:21 +0530

    mhm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import AddComponent from './Components/AddComponent';
 import HomeComponent from './Components/HomeComponent';
 import ListComponent from './Components/ListComponent';
+import Navbar from './Components/Navbar/Navbar';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
 function App() {
   const [trip, setTrip] = useState([]);
@@ -15,11 +17,17 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <HomeComponent trips={trip}/>
-      <AddComponent onAdd={addTrip}/>
-      <ListComponent triplist={trip}/>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/"><HomeComponent trips={trip}/></Route>
+          <Route path="/Home"><HomeComponent trips={trip}/></Route>
+          <Route path="/Add"><AddComponent onAdd={addTrip}/></Route>
+          <Route path="/List"><ListComponent triplist={trip}/></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { BiHomeAlt, BiCalendarPlus } from 'react-icons/bi';
+import { FaCalendarAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="side-nav">
+            <div className="nav-bar">
+                <li className="nav-item"><Link to="/"><BiHomeAlt /></Link></li>
+                <li className="nav-item"><Link to="/Add"><BiCalendarPlus /></Link></li>
+                <li className="nav-item"><Link to="/List"><FaCalendarAlt /></Link></li>
+
+
+            </div>
         </div>
     )
 }
