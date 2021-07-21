11d074db698756cc77a04f1329003ab3d772d968
Narasimha Prasad <ynprasadkklreddy2@gmail.com>
2021-07-21 09:56:02 +0530

    filter remaining

--- src/Components/AddComponent.jsx
+++ src/Components/AddComponent.jsx
@@ -5,65 +5,69 @@ class AddComponent extends Component {
         super(props)
 
         this.state = {
-            date:'',
-            place:'',
-            type:'Tropic'
-            
+            date: '',
+            place: '',
+            type: 'Tropic'
+
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event){
-        const {name, value} = event.target;
+    handleChange(event) {
+        const { name, value } = event.target;
 
         this.setState((prevState) => {
-            return{
+            return {
                 ...prevState,
-                [name]:value
+                [name]: value
             }
         });
     }
 
-    handleSubmit(event){
+    handleSubmit(event) {
         this.props.onAdd(this.state)
         console.log(this.state);
         this.setState({
-            date:'',
-            place:'',
-            type:'Tropic'
+            date: '',
+            place: '',
+            type: 'Tropic'
         });
         event.preventDefault();
     }
 
     render() {
         return (
-            
+
+            <div className="container">
                 <div className="form-container">
+                    <div className="form-heading"><p>Add a Trip</p></div>
                     <div className="trip-form">
                         <form onSubmit={this.handleSubmit}>
-                            <label>
-                            Date:
-                            <input type="date" name="date" value={this.state.date} onChange={this.handleChange}/>
+                            <label style={{ color: '#FFF' }}>
+                                Date:
                             </label>
-                            <label>
-                            Place:
-                            <input type="text" name="place" value={this.state.place} onChange={this.handleChange}/>
+                            <input type="date" name="date" value={this.state.date} onChange={this.handleChange} />
+                            <label style={{ color: '#FFF' }}>
+                                Place:
+                            <input type="text" name="place" value={this.state.place} onChange={this.handleChange} />
                             </label>
-                            <label>
-                            type:
+                            <label style={{ color: '#FFF' }}>
+                                type:
                             <select value={this.state.type} name="type" onChange={this.handleChange}>
-                                <option value="Tropic" defaultValue>Tropic</option>
-                                <option value="Trek">Trek</option>
-                                <option value="Club">Club</option>
-                            </select>
+                                    <option value="Tropic" defaultValue>Tropic</option>
+                                    <option value="Trek">Trek</option>
+                                    <option value="Club">Club</option>
+                                </select>
                             </label>
                             <input type="submit" value="Submit" />
                         </form>
                     </div>
-
                 </div>
-            
+
+
+            </div>
+
         )
     }
 }
--- src/Components/ListComponent.jsx
+++ src/Components/ListComponent.jsx
@@ -1,14 +1,35 @@
 import React, { PureComponent } from 'react'
 
 class ListComponent extends PureComponent {
-   
+
 
     render() {
         return (
-            <div>
-            {this.props.triplist.map((trip,index) => {
-                return <ul key={index}>{trip.date} {trip.place}  {trip.type}</ul>
-            })}
+            <div className="container-1">
+                <div className="list-container">
+                    <div className="list-heading">All Trips</div>
+                    <div className="list-content">
+                        <div className="content-heading">
+                            <div className="heading-items">Date</div>
+                            <div className="heading-items">Place</div>
+                            <div className="heading-items">Type</div>
+                        </div>
+                        <div className="list-items">
+                            {this.props.triplist.map((trip, index) => {
+                                return (
+                                    <div className="main-list" key={index}>
+                                        <div className="main-item">{trip.date}</div>
+                                        <div className="main-item">{trip.place}</div>
+                                        <div className="main-item">{trip.type}</div>
+                                    </div>
+                                )
+                            })
+                            }
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
         )
     }
